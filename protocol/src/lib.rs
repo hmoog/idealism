2dd44a508500a3e86affc4488483feaa95f30ff5
Hans Moog <3293976+hmoog@users.noreply.github.com>
2025-05-08 17:59:34 +0200

    moved protocol to own file

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -2,35 +2,6 @@ mod config;
 mod managed_plugin;
 mod plugin;
 mod plugins;
+mod protocol;
 
-use std::sync::Arc;
-
-pub use config::ProtocolConfig;
-pub use managed_plugin::ManagedPlugin;
-pub use plugin::Plugin;
-pub use plugins::Plugins;
-use tracing::info;
-
-pub struct Protocol {
-    pub plugins: Plugins,
-}
-
-impl Protocol {
-    pub fn new(config: impl ProtocolConfig) -> Self {
-        let mut plugins = Plugins::default();
-        Self {
-            plugins: ProtocolConfig::inject_plugins(&*plugins.provide(Arc::new(config)), plugins),
-        }
-    }
-
-    pub async fn start(&self) {
-        info!(target: "protocol", "started");
-        self.plugins.start().await;
-        info!(target: "protocol", "stopped");
-    }
-
-    pub fn shutdown(&self) {
-        info!(target: "protocol", "shutting down");
-        self.plugins.shutdown();
-    }
-}
+pub use crate::{config::*, managed_plugin::*, plugin::*, plugins::*, protocol::*};
---
+++ protocol/src/protocol.rs
@@ -0,0 +1,29 @@
+use std::sync::Arc;
+
+use tracing::info;
+
+use crate::{Plugins, ProtocolConfig};
+
+pub struct Protocol {
+    pub plugins: Plugins,
+}
+
+impl Protocol {
+    pub fn new(config: impl ProtocolConfig) -> Self {
+        let mut plugins = Plugins::default();
+        Self {
+            plugins: ProtocolConfig::inject_plugins(&*plugins.provide(Arc::new(config)), plugins),
+        }
+    }
+
+    pub async fn start(&self) {
+        info!(target: "protocol", "started");
+        self.plugins.start().await;
+        info!(target: "protocol", "stopped");
+    }
+
+    pub fn shutdown(&self) {
+        info!(target: "protocol", "shutting down");
+        self.plugins.shutdown();
+    }
+}
